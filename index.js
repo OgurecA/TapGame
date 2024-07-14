61db1b2aef92e8c0faf1d369c9a7daff6f988cd5
OgurecA <acpwork044@gmail.com>
2024-07-14 16:48:51 +0400

    k

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.post('/hook', async (req, res) => {
 		
         // Логика ответа на команду /start
         if (text === '/start') {
-            await sendMessage('Привет, это ваш Telegram бот!'); // Отправляем приветственное сообщение
+            await sendMessage(chatId, 'Привет, это ваш Telegram бот!');
         }
 
         res.status(200).send('Received');
